40be4db4d063aa619c7b04ba20735dc152cf6ab8
stephenmdevine <stephenmdevine@gmail.com>
2024-04-30 22:41:53 -0500

    Initialization

---
+++ src/main/java/org/devine/aberrant/AberrantApplication.java
@@ -0,0 +1,13 @@
+package org.devine.aberrant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AberrantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AberrantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/devine/aberrant/AberrantApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devine.aberrant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AberrantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
