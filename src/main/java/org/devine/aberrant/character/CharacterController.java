f761a5f77da26001511b66157a1e612c0c0f7caa
stephenmdevine <stephenmdevine@gmail.com>
2024-05-18 23:02:27 -0500

    Minor Controller updates

--- src/main/java/org/devine/aberrant/character/CharacterController.java
+++ src/main/java/org/devine/aberrant/character/CharacterController.java
@@ -2,6 +2,7 @@ package org.devine.aberrant.character;
 
 import org.devine.aberrant.ability.AllocateAbilityPointsRequest;
 import org.devine.aberrant.attribute.AllocateAttributePointsRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,7 @@ import java.util.Map;
 @RequestMapping("/characters")
 public class CharacterController {
 
+    @Autowired
     private final CharacterService characterService;
 
     public CharacterController(CharacterService characterService) {
